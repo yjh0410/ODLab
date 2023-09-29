dcc6cb685804d0713c7aea0329785e0e7cd3cd41
yjh0410 <1394571815@qq.com>
2023-09-29 16:03:42 +0800

    add FCOS 3x

--- evaluator/__init__.py
+++ evaluator/__init__.py
@@ -1,9 +1,8 @@
-import os
-
 from evaluator.coco_evaluator import COCOAPIEvaluator
 
 
 def build_evluator(args, cfg, device, testset=False):
+    evaluator = None
     # COCO Evaluator
     if args.dataset == 'coco':
         evaluator = COCOAPIEvaluator(args, cfg, device, testset)
