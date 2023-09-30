a0e38000bd7afee6fd57423afcb67bea52ebbcc8
yjh0410 <1394571815@qq.com>
2023-09-30 09:56:01 +0800

    update

--- train_multi_gpu.sh
+++ train_multi_gpu.sh
@@ -1,2 +1,2 @@
 # 8 GPUs
-python -m torch.distributed.run --nproc_per_node=8 train.py --cuda -dist -d coco --root /data/datasets/COCO/ -m fcos_r18_1x --batch_size 16 --eval_epoch 2 
+python -m torch.distributed.run --nproc_per_node=4 train.py --cuda -dist -d coco --root /data/datasets/COCO/ -m fcos_r18_1x --batch_size 16 --eval_epoch 2 
