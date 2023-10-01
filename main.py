2340478a2eefe19415a8866aad40f938709d68e4
yjh0410 <1394571815@qq.com>
2023-10-01 21:54:26 +0800

    update

--- main.py
+++ main.py
@@ -86,9 +86,8 @@ def main():
     os.makedirs(path_to_save, exist_ok=True)
 
     # ---------------------------- Build DDP ----------------------------
-    if args.distributed:
-        distributed_utils.init_distributed_mode(args)
-        print("git:\n  {}\n".format(distributed_utils.get_sha()))
+    distributed_utils.init_distributed_mode(args)
+    print("git:\n  {}\n".format(distributed_utils.get_sha()))
     world_size = distributed_utils.get_world_size()
     print('World size: {}'.format(world_size))
     per_gpu_batch = args.batch_size // world_size
