fd47105b37114c666e1bd88ec174e1a031f9e17e
yjh0410 <1394571815@qq.com>
2023-10-02 09:10:06 +0800

    add demo

--- utils/vis_tools.py
+++ utils/vis_tools.py
@@ -42,6 +42,42 @@ def vis_data(images, targets):
         cv2.imshow('train target', image)
         cv2.waitKey(0)
 
+## plot bbox & label on image
+def plot_bbox_labels(img, bbox, label=None, cls_color=None, text_scale=0.4):
+    x1, y1, x2, y2 = bbox
+    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+    t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
+    # plot bbox
+    cv2.rectangle(img, (x1, y1), (x2, y2), cls_color, 2)
+    
+    if label is not None:
+        # plot title bbox
+        cv2.rectangle(img, (x1, y1-t_size[1]), (int(x1 + t_size[0] * text_scale), y1), cls_color, -1)
+        # put the test on the title bbox
+        cv2.putText(img, label, (int(x1), int(y1 - 5)), 0, text_scale, (0, 0, 0), 1, lineType=cv2.LINE_AA)
+
+    return img
+
+## visualize detection
+def visualize(img, 
+              bboxes, 
+              scores, 
+              labels, 
+              vis_thresh, 
+              class_colors, 
+              class_names):
+    ts = 0.4
+    for i, bbox in enumerate(bboxes):
+        if scores[i] > vis_thresh:
+            cls_id = int(labels[i])
+            cls_color = class_colors[cls_id]
+                
+            mess = '%s: %.2f' % (class_names[cls_id], scores[i])
+            img = plot_bbox_labels(img, bbox, mess, cls_color, text_scale=ts)
+
+    return img
+        
+
 ## convert feature to he heatmap
 def convert_feature_heatmap(feature):
     """
