22f0aaa98d5d160c96ea26cf32f10d29e9352cd4
yjh0410 <1394571815@qq.com>
2023-10-02 08:40:29 +0800

    train FCOS-R50-1x

--- train_multi_gpu.sh
+++ train_multi_gpu.sh
@@ -1,2 +1,2 @@
 # 8 GPUs
-python -m torch.distributed.run --nproc_per_node=2 main.py --cuda -dist -d coco --root /data/datasets/COCO/ -m fcos_r18_3x --batch_size 16 --eval_epoch 2
+python -m torch.distributed.run --nproc_per_node=8 main.py --cuda -dist -d coco --root /data/datasets/COCO/ -m fcos_r50_1x --batch_size 16 --eval_epoch 2
