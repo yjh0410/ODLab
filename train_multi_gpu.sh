7f2c976a9638856f89b14c4c745d2d7d567e5b3e
yjh0410 <1394571815@qq.com>
2023-10-10 09:21:53 +0800

    train FCOS-RT-R18 on COCO

--- train_multi_gpu.sh
+++ train_multi_gpu.sh
@@ -1,2 +1,2 @@
 # 8 GPUs
-python -m torch.distributed.run --nproc_per_node=2 --master_port 1663 main.py --cuda -dist -d coco --root /data/datasets/COCO/ -m fcos_r18_rt_3x --batch_size 16 --eval_epoch 2
+python -m torch.distributed.run --nproc_per_node=2 --master_port 1663 main.py --cuda -dist -d coco --root /data/datasets/COCO/ -m fcos_rt_r18_3x --batch_size 16 --eval_epoch 2
