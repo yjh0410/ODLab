3a83fcf549a0a09db3cf79ae0f8cdf086bb2be85
yjh0410 <1394571815@qq.com>
2023-10-14 08:35:37 +0800

    train RetinaNet-R18-1x

--- train_multi_gpu.sh
+++ train_multi_gpu.sh
@@ -1,2 +1,2 @@
 # 8 GPUs
-python -m torch.distributed.run --nproc_per_node=8 --master_port 1663 main.py --cuda -dist -d coco --root /data/datasets/COCO/ -m yolof_r50_c5_1x --batch_size 64 --eval_epoch 2
+python -m torch.distributed.run --nproc_per_node=8 --master_port 1663 main.py --cuda -dist -d coco --root /data/datasets/COCO/ -m retinanet_r18_1x --batch_size 16 --eval_epoch 2
