57cd117bd918fd2d6b64fd53cfc63ca9f320104a
yjh0410 <1394571815@qq.com>
2023-10-21 13:32:12 +0800

    add SwinTR

--- models/backbone/__init__.py
+++ models/backbone/__init__.py
@@ -1,12 +1,16 @@
 from .resnet import build_resnet
+from .swin_transformer import built_swin_transformer
 
 
 def build_backbone(cfg, pretrained=False):
     print('==============================')
     print('Backbone: {}'.format(cfg['backbone']))
-    
+    # ResNet
     if cfg['backbone'] in ['resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152']:
         pretrained_weight = cfg['pretrained_weight'] if pretrained else None
         return build_resnet(cfg, pretrained_weight)
+    # SwinTransformer
+    elif cfg['backbone'] in ['swin_t', 'swin_s', 'swin_b', 'swin_l']:
+        return built_swin_transformer(cfg, pretrained)
     
                            
