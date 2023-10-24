7c55ec6d5484a2496a28a0e57d2c077e1523b1c3
yjh0410 <1394571815@qq.com>
2023-10-24 08:21:31 +0800

    train PlainDETR with Hybrid matching

--- models/transformer/__init__.py
+++ models/transformer/__init__.py
@@ -1,23 +1,8 @@
-from .transformer import DETRTransformer, PlainDETRTransformer
+from .transformer import PlainDETRTransformer
 
 
 def build_transformer(cfg, num_classes, trainable=False, return_intermediate=False):
-    if cfg['transformer'] == "detr_transformer":
-        return DETRTransformer(d_model             = cfg['d_model'],
-                               num_encoder         = cfg['num_encoder'],
-                               encoder_num_head    = cfg['encoder_num_head'],
-                               encoder_mlp_ratio   = cfg['encoder_mlp_ratio'],
-                               encoder_dropout     = cfg['encoder_dropout'],
-                               encoder_act_type    = cfg['encoder_act'],
-                               num_decoder         = cfg['num_decoder'],
-                               decoder_num_head    = cfg['decoder_num_head'],
-                               decoder_mlp_ratio   = cfg['decoder_mlp_ratio'],
-                               decoder_dropout     = cfg['decoder_dropout'],
-                               decoder_act_type    = cfg['decoder_act'],
-                               num_classes         = num_classes,
-                               return_intermediate = return_intermediate
-                               )
-    elif cfg['transformer'] == "plain_detr_transformer":
+    if cfg['transformer'] == "plain_detr_transformer":
         return PlainDETRTransformer(is_train            = trainable,
                                     d_model             = cfg['d_model'],
                                     num_encoder         = cfg['num_encoder'],
