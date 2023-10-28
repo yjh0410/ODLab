fab77fac92cb016029f5932c712faa449c3fb262
yjh0410 <1394571815@qq.com>
2023-10-28 19:07:37 +0800

    train PlainDETR with R50-v2

--- train_multi_gpu.sh
+++ train_multi_gpu.sh
@@ -1,6 +1,6 @@
 # 8 GPUs
 python -m torch.distributed.run \
-                    --nproc_per_node=2 \
+                    --nproc_per_node=8 \
                     --master_port 1663 \
                     main.py \
                     --cuda \
@@ -8,6 +8,6 @@ python -m torch.distributed.run \
                     -d coco \
                     --root /data/datasets/COCO/ \
                     -m pdetr_r50_1x \
-                    --batch_size 4 \
+                    --batch_size 16 \
                     --eval_epoch 2 \
                     # --resume weights/coco/pdetr_r18_1x/pdetr_r18_1x_best.pth
